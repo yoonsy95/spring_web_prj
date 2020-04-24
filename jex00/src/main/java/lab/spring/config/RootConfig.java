50484dc5ad69efb722c5dd626b1494d36ddbebad
yoonsy95 <yegyo95@gmail.com>
2020-04-25 06:53:02 +0900

    char01.03 oracle database

--- jex00/src/main/java/lab/spring/config/RootConfig.java
+++ jex00/src/main/java/lab/spring/config/RootConfig.java
@@ -1,10 +1,27 @@
 package lab.spring.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = "{lab.spring.sample}")
 public class RootConfig {
-
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
 }
