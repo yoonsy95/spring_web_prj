c00ea6c414e4370cc16c78809ff3f395a1123602
yoonsy95 <yegyo95@gmail.com>
2020-05-02 01:26:41 +0900

    chap.05.01

--- jex01/src/main/java/lab/spring/config/WebConfig.java
+++ jex01/src/main/java/lab/spring/config/WebConfig.java
@@ -8,11 +8,13 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 		return new Class[] {RootConfig.class};
 	}
 
+//	ServletConfig 클래수 정상적으로 실행 위하여
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] {ServletConfig.class};
 	}
 
+//	spring mvc 기본 경로
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
